63b2001fa899618a4217229c526524519f6584cc
Andrea Viviani <andrea.viviani89@gmail.com>
2022-07-15 13:37:05 +0200

    creating a structure

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+# create an instance
+app = Flask(__name__)
+
+@app.route("/") ## Decorator 
+def index():
+    return render_template('home.html')
+
+
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True)
